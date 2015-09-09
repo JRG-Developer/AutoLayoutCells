92b604cb913269707b9f9727505d894793f1a214
Joshua Greene <joshua.greene@citrix.com>
2015-09-08 22:20:32 -0500

    Fixed header comments

--- AutoLayoutCells/TableViewCells/ALTextCellViewModel.h
+++ AutoLayoutCells/TableViewCells/ALTextCellViewModel.h
@@ -1,6 +1,6 @@
 //
-//  TextCellViewModel.h
-//  CityScavengerHunt
+//  ALTextCellViewModel.h
+//  AutoLayoutCells
 //
 //  Created by Joshua Greene on 7/17/15.
 //  Copyright (c) 2015 JRG-Developer. All rights reserved.
--- AutoLayoutCells/TableViewCells/ALTextCellViewModel.m
+++ AutoLayoutCells/TableViewCells/ALTextCellViewModel.m
@@ -1,6 +1,6 @@
 //
-//  TextCellViewModel.m
-//  CityScavengerHunt
+//  ALTextCellViewModel.h
+//  AutoLayoutCells
 //
 //  Created by Joshua Greene on 7/17/15.
 //  Copyright (c) 2015 JRG-Developer. All rights reserved.
